8fb7aebfd110723e0239e8af44128e7356e84a26
Juan Ludevid <jmludevid@hotmail.com>
2024-04-26 11:02:01 +1000

    update useAnimation hook

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,28 +1,42 @@
+/* eslint-disable react/no-unstable-nested-components */
 import {useQuery} from '@tanstack/react-query';
 import React from 'react';
-import {View} from 'react-native';
-import {ActivityIndicator, Button, Text} from 'react-native-paper';
+import {FlatList, StyleSheet, View} from 'react-native';
+
 import {getPokemons} from '../../../actions/pokemons';
+import {PokeballBG} from '../../components/ui/PokeballBG';
+import {Text} from 'react-native-paper';
+import {globalTheme} from '../../../config/theme/global-theme';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+
+import {PokemonCard} from '../../components/pokemons/PokemonCard';
 
 export const HomeScreen = () => {
-  const {isLoading, data} = useQuery({
+  const {top} = useSafeAreaInsets();
+  const {isLoading, data: pokemons = []} = useQuery({
     queryKey: ['pokemons'],
     queryFn: () => getPokemons(0),
     staleTime: 1000 * 60 * 60, // 60 minutes
   });
   return (
-    <View>
-      <Text variant="displaySmall">HomeScreen</Text>
-      {isLoading ? (
-        <ActivityIndicator />
-      ) : (
-        <>
-          <View>{JSON.stringify(data)}</View>
-          <Button mode="contained" onPress={() => console.log('Pressed')}>
-            Press me
-          </Button>
-        </>
-      )}
+    <View style={globalTheme.globalMargin}>
+      <PokeballBG style={styles.imgPosition} />
+      <FlatList
+        data={pokemons}
+        keyExtractor={(pokemon, index) => `${pokemon.id}-${index}`}
+        numColumns={2}
+        style={{paddingTop: top + 20}}
+        ListHeaderComponent={() => <Text variant="displayMedium">Pokedex</Text>}
+        renderItem={({item}) => <PokemonCard pokemon={item} />}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  imgPosition: {
+    position: 'absolute',
+    top: -100,
+    right: -100,
+  },
+});
